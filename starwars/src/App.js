6364a8394b916ffc5ee927743f172d9b1fa505e6
Landon Cuff <landoncuff23@gmail.com>
2022-02-01 14:35:05 -0700

    Getting the Data

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import {useState, useEffect} from 'react'
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +11,39 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  //* creating state
+  const [charactersData, setCharacters] = useState([])
+
+  //* useEffect() is ran everytime the page is rendered (opened) or refreshed
+  useEffect(() => {
+
+    //* calling an async function so it is ran and the other code wont wait 
+    async function getData(){
+      //* waiting for the data to be sent back
+      const response = await axios.get('https://swapi.dev/api/people')
+
+      //* putting the response into state
+      setCharacters(response.data.results)
+    }
+    getData()
+
+  }, [])
+
+  useEffect(() => {
+    console.log(charactersData);
+  }, [charactersData]);
+
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <ul>
+        {charactersData.map((char) => {
+          return (
+            <li>{char.name}</li>
+          )
+        })}
+      </ul>
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,8 @@
 // Write your Character component here
+export default function Character({charData}){
+    return (
+        <div>
+            <li>{charData.name}</li>
+        </div>
+    )
+}
